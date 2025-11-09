8b9965320c7a7e416422ae21e581cd3f56d026f4
Nidhikushwaha12 <nidhikush2008@gmail.com>
2025-11-09 11:27:01 +0000

    variable, datatype, operations

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accounId=12334
+let accountEmail="nidhi@gmail.com"
+var accountPassword="12345"
+accountCity="New Delhi"
+
+accountEmail="tau@gmail.com"
+accountPassword="34543"
+accountCity="Noida"
+
+/*prefer not to use var because of issue in block scope and functional scope*/ 
+
+console.log(accounId);
+console.table([accountEmail,accountPassword,accountCity ]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+//alert (3+3) // we can use alert only in browser not in node js
+
+console.log(3+3);// we can use console in both browser and node js
+console.log("hello world");//string datatype
+
+let name="nidhi"
+let age=22
+let isLoggedIn = false 
+
+
+//nmber=> 2 to power 53
+//bigint => larger than 2 to power 53
+//string => ''or""
+//boollean => true/false
+//null => standalone value or null is object in js
+//undefined => undefined value
+//symbol => unique identifier
+
+
+//object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
