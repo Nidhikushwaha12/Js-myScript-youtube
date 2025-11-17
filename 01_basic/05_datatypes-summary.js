ab16c7625bc227a88d78b8b96a0bb9631705e19e
NidhiKushwaha <nidhikush2008@gmail.com>
2025-11-17 03:14:17 +0000

    datatypes

--- 01_basic/05_datatypes-summary.js
+++ 01_basic/05_datatypes-summary.js
@@ -31,3 +31,30 @@ let myObj={
 console.log(typeof heros)
 
 //JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution.
+
+
+//   ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//stack(primitive) #it gives always a copy that's why value is changable ,
+// heap(non-primitive) #it gives a reference not a copy and value can't be change cuz it's not copy .it's a reference
+
+let youtubeName ="hiteshchoudhary"
+
+let anotherName = youtubeName
+anotherName = "chaiorcode"
+
+console.log(anotherName)
+console.log(youtubeName)
+
+let userOne = {
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email )
